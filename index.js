ea3532f1577ec29436497ceed15193cd8b86e8f2
xixi050330 <ferdialwan050330@gmail.com>
2024-06-28 13:59:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var nama = (prompt("namanya siapaa??"));
 if (nama == "wanda") {
     alert("halooo " + nama + " sayangg");
 } else if (nama == "anda") {
-    alert("halooo" + nama + " sayangg");
+    alert("halooo " + nama + " sayangg");
 } else {
     alert("salah nama kalik");
 }

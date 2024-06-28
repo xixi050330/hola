f2fa9c980e08dab92bfbc1a6aa5402b2dc1153c2
xixi050330 <ferdialwan050330@gmail.com>
2024-06-28 13:56:56 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Haloo kamuu");
+var nama = (prompt("namanya siapaa??"));
+
+if (nama == "wanda") {
+    alert("halooo " + nama + " sayangg");
+} else if (nama == "anda") {
+    alert("halooo" + nama + " sayangg");
+} else {
+    alert("salah nama kalik");
+}
